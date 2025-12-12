1a8d5e6f813e5c78792491ee369febd4ad62a89f
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-12-12 14:42:13 +0800

    Update the find_largest_joultage method

--- Day 3/Problem 1.py
+++ Day 3/Problem 1.py
@@ -10,7 +10,23 @@ def extract_banks_from_file(filename):
     return result
 
 def find_largest_joultage(bank):
-    return 0
+    largest_battery_index = 0
+
+    # -1 as we do not want to loop to the last element
+    for i in range(len(bank) - 1):
+        if bank[i] > bank[largest_battery_index]:
+            largest_battery_index = i
+
+    second_largest_battery_index = largest_battery_index + 1
+    for i in range(second_largest_battery_index, len(bank)):
+        if bank[i] > bank[second_largest_battery_index]:
+            second_largest_battery_index = i
+
+    digit_str = bank[largest_battery_index] + bank[second_largest_battery_index]
+    digit = int(digit_str)
+
+    return digit
+
 
 def main():
     banks = extract_banks_from_file('input/sample.txt')
