06cf40b78682da08822f4e326bd7c8e14a852765
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-12-12 17:51:17 +0800

    Add skeleton for problem 1 day 4

--- Day 4/Problem 1.py
+++ Day 4/Problem 1.py
@@ -1,5 +1,18 @@
+def get_adjacency_list_from_file(filename):
+    return {
+        'key': []
+    }
+
 def main():
-    print("Hello World")
+    adjacency_list = get_adjacency_list_from_file('inputs/sample.txt')
+
+    no_rolls_can_remove = 0
+
+    for k, neighbours in adjacency_list.items():
+        if len(neighbours) < 4:
+            no_rolls_can_remove += 1
+
+    print(no_rolls_can_remove)
 
 if __name__ == '__main__':
     main()
