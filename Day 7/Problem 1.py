dc24833632139c3f7f98276bb4095d1b9213cdb3
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-12-15 20:21:09 +0800

    Add read_diagram_from_filename method

--- Day 7/Problem 1.py
+++ Day 7/Problem 1.py
@@ -1,5 +1,15 @@
+def read_diagram_from_filename(filename):
+    diagram = []
+
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            str = line.strip()
+            diagram.append(str)
+
+    return diagram
+
 def main():
-    print("Hello World")
+    diagram = read_diagram_from_filename('input/sample')
 
 if __name__ == '__main__':
     main()
