36cd34fd5c16ea8ea014977e785b05f47dffe373
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-12-16 13:59:37 +0800

    Add get_euclidean_distance_between_points method

--- Day 8/Problem 1.py
+++ Day 8/Problem 1.py
@@ -1,3 +1,6 @@
+import math
+
+
 def get_junction_boxes(filename):
     junction_boxes = []
 
@@ -8,6 +11,15 @@ def get_junction_boxes(filename):
             junction_boxes.append((int(x), int(y), int(z)))
 
     return junction_boxes
+
+
+def get_euclidean_distance_between_points(point_1, point_2):
+    x1, y1, z1 = point_1
+    x2, y2, z2 = point_2
+
+    sub_total = math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) + math.pow(z2 - z1, 2)
+
+    return math.sqrt(sub_total)
 
 
 def main():
