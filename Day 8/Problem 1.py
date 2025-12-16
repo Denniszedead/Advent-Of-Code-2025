5fa6a372b61ea116cb0cd5b94cda848dfa85c9ab
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-12-16 13:53:48 +0800

    Add get_junction_boxes function

--- Day 8/Problem 1.py
+++ Day 8/Problem 1.py
@@ -1,5 +1,17 @@
+def get_junction_boxes(filename):
+    junction_boxes = []
+
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            cord_str = line.strip()
+            x, y, z = cord_str.split(',')
+            junction_boxes.append((int(x), int(y), int(z)))
+
+    return junction_boxes
+
+
 def main():
-    pass
+    junction_boxes = get_junction_boxes('input/sample')
 
 
 if __name__ == '__main__':
