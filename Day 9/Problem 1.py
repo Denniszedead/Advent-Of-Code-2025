cee94e358c0a1514ee2f89a62e0fd715b9527ebd
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-12-17 16:02:00 +0800

    Add get_points_from_file method

--- Day 9/Problem 1.py
+++ Day 9/Problem 1.py
@@ -1,5 +1,19 @@
+def get_points_from_file(filename):
+    points = []
+
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            point_str = line.strip()
+            x_str, y_str = point_str.split(',')
+            x = int(x_str)
+            y = int(y_str)
+            points.append((x, y))
+
+    return points
+
+
 def main():
-    pass
+    points = get_points_from_file('input/sample')
 
 
 if __name__ == "__main__":
